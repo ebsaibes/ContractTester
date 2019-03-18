37d6274c0d556064ac4fd813f42d23922bf4d431
ebsaibes2 <emil.bsaibes@headspring.com>
2019-03-18 08:03:42 -0500

    add TestMessage view Model

---
+++ ContractTester/ViewModels/TestMessageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Threading.Tasks;
+using ContractTester.Models;
+using Contract = ContractTester.Models.Contract;
+
+namespace ContractTester.ViewModels
+{
+    public class TestMessageViewModel
+    {
+        public Contract contract { get; set; }
+        public bool TestMessageIsValid { get; set; }
+        public string TestMessage { get; set; }
+
+        public TestMessageViewModel() { }
+    }
+
+}
